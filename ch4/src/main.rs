cc09ebf7236c34ef38cedf38ffbc3aaed72ca439
Louca <loucamorales@orange.fr>
2026-01-27 11:33:54 -0500

    finish ch4 add detail

--- ch4/src/main.rs
+++
@@ -1,9 +0,0 @@
-//learn gestionary of heap
-fn main() {
-    let mut s = String::from("test");
-    let copy;
-    s.push_str(" of test 0");
-    copy = s;
-    //after s cannot be used
-    println!("{} / test1", s);
-}
--- ch4_1/src/main.rs
+++ ch4_1/src/main.rs
@@ -1,9 +1,14 @@
 //learn gestionary of heap
 fn main() {
+    let infinite = Box::new([100; 1000_000]);
     let mut s = String::from("test");
     let copy;
+    println!("{:?}", infinite);
+    let rob = infinite;
+    println!("{:?}", rob);
     s.push_str(" of test 0");
+    
     copy = s;
     //after s cannot be used
-    println!("{} / test1", s);
+    println!("{} / test1", copy);
 }
