6c684301e1f9fb965d6fe3908b2041120d93a801
Louca <loucamorales@orange.fr>
2026-01-28 06:19:56 -0500

    learn reference and infinite return ch4_2

--- ch4_2/src/main.rs
+++ ch4_2/src/main.rs
@@ -1,12 +1,32 @@
 fn main() {
+    learn_both_return();
+    learn_references();
+}
+
+fn learn_both_return(){
+    println!("----------------------------------------------------------------\nBoth return");
     let start = String::from("Helloy /");
     let next = String::from("worldo !!!");
-    let (start1, next1) = greet(start, next);
+    let (start1, next1) = greet_return(start, next);
     let test = format!("{} {}", start1, next1);
     println!("{}", test);
 }
 
-fn greet(p_start: String, p_end: String) -> (String, String){
+fn learn_references(){
+    println!("----------------------------------------------------------------\nlearn references");
+    let start = String::from("Helloy /");
+    let next = String::from("worldo !!!");
+    greet_references(&start, &next);
+    let test = format!("{} {}", start, next);
+    println!("{}", test);
+
+}
+
+fn greet_return(p_start: String, p_end: String) -> (String, String){
     println!("{} {}", p_start, p_end);
     (p_start, p_end)
+}
+
+fn greet_references(p_start: &String, p_end: &String){
+    println!("{} {}", p_start, p_end);
 }
